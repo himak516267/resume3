173bfd82000892b68b9fb1527ef39f8dde89ea86
himak516267 <manchureddy143@gmail.com>
2018-07-31 16:21:54 +0530

    hi1231

---
+++ main.js
@@ -0,0 +1,37 @@
+//{
+    //var xhr=new XMLHttpRequest();
+  //  xhr.overrideMimeType("application/JSON");
+  //  xhr.open("GET",file+true);
+  //  xhr.onreadystatechange=function()
+  //  {
+    //    if(xhr.onreadystate===4&&xhr.status=="200");
+    //      {
+    //              callback(xhr.responceText);
+    //        }
+  //    }
+    //      xhr.send();
+    //  }
+    //  loadJSON("data.jsan",function)
+    function loadJSON(file){
+      return new Promise((resolove,request)=>{
+        return fetch(file).then(responce=>{
+          if (responce.ok) {
+            resolove(responce.json());
+          }
+            else{
+              reject(new error("error"));
+            }
+        })
+      })
+    }
+var fetchedData=loadJSON("java.json");
+fetchedData.then(data=>{
+  console.log(data);
+  career(data.career);
+})
+var child2=document.querySelector("#child2");
+function career(car){
+  var p=document.createElement("p");
+  p.textContent=car.info;
+  child2.appendChild(p);
+}
