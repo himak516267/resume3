3e5ac39792cdaafe7f9305ef921650d515c7e211
himak516267 <manchureddy143@gmail.com>
2018-08-01 11:09:49 +0530

    13log

--- main.js
+++ main.js
@@ -28,13 +28,27 @@ var fetchedData=loadJSON("java.json");
 fetchedData.then(data=>{
   console.log(data);
   career(data.career);
+  education(data.career);
 })
 var child2=document.querySelector("#child2");
 function career(car){
   var heading=document.createElement("h2");
   heading.textContent="career objective";
   child2.appendChild(heading);
+  var hLine=document.createElement("hr");
+  child2.appendChild(hLine);
   var p=document.createElement("p");
   p.textContent=car.info;
   child2.appendChild(p);
+}
+function education(edu){var heading=document.createElement("h2");
+heading.textContent="education qualifition";
+child2.appendChild(heading);
+var hLine=document.createElement("hr");
+child2.appendChild(hLine);
+var table=document.createElement("table");
+child2.appendChild(table);
+var tr="<tr><td>S.no</td><td>degree</td><td>institute</td><td>data</td></tr>";
+table.innerHTML=tr;
+table.border="1";
 }
