e678dd1386ddc1e2eb1ca554cd618e55361f7923
himak516267 <manchureddy143@gmail.com>
2018-08-01 13:00:34 +0530

    13l2og

--- main.js
+++ main.js
@@ -25,10 +25,11 @@
       })
     }
 var fetchedData=loadJSON("java.json");
-fetchedData.then(data=>{
-  console.log(data);
-  career(data.career);
-  education(data.career);
+fetchedData.then(java=>{
+  console.log(java);
+  career(java.career);
+  education(java.education);
+  skills(java.skills);
 })
 var child2=document.querySelector("#child2");
 function career(car){
@@ -41,7 +42,9 @@ function career(car){
   p.textContent=car.info;
   child2.appendChild(p);
 }
-function education(edu){var heading=document.createElement("h2");
+function education(edu)
+{
+  var heading=document.createElement("h2");
 heading.textContent="education qualifition";
 child2.appendChild(heading);
 var hLine=document.createElement("hr");
@@ -49,6 +52,26 @@ child2.appendChild(hLine);
 var table=document.createElement("table");
 child2.appendChild(table);
 var tr="<tr><td>S.no</td><td>degree</td><td>institute</td><td>data</td></tr>";
-table.innerHTML=tr;
+//table.innerHTML=tr;
 table.border="1";
+var tr1="";
+for(var i=0;i<edu.length;i++){
+tr1+="<tr><td>"+(i+1)+"</td><td>"+edu[i].degree+"</td><td>"+edu[i].institute+"</td><td>"+edu[i].data+"</td></tr>";
+}
+table.innerHTML=tr+tr1;
+}
+function skills(skills)
+{
+  var heading=document.createElement("h2");
+  heading.textContent="technical skills ";
+  child2.appendChild(heading);
+  var hLine=document.createElement("hr");
+  heading.appendChild(hLine);
+  for(var i=0;i<skills.length;i++){
+    var title=document.createElement("h4");
+    title.textContent=skills[i].title;
+    child2.appendChild(title);
+    var list=document.createElement("ul");
+    child2.appendChild(list);
+  }
 }
